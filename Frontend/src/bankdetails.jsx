159f2ac9a7598dc941c1c84622f55fba38575d69
Venkateshwaran-SakthiMurugan <venkateshwarangs@student.tce.edu>
2025-03-22 18:05:59 +0530

    bankdetails name changed

--- Frontend/src/bankdetails.jsx
+++
@@ -1,9 +0,0 @@
-import { Box } from "@chakra-ui/react"
-export function BankDetails() {
-    return (
-      <Box cursor="pointer" bg="bg" shadow="md" borderRadius="md">
-        Box with shadow
-      </Box>
-
-    )
-}
